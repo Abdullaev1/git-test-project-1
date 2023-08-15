20226b7fb5a7e351178c67e7df266a78c76e723a
Oskar <abdullaev122@vk.com>
2023-08-15 11:21:27 +0300

    change line 14

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Py')
-    print_hi('Charm')
+    print_hi('Charm1')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
